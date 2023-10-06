60ee43d3317beaf0f530332f7c42a65211f3d77d
onixgoos <onixgoos@student.21-school.ru>
2023-10-06 20:23:10 +0300

    onixgoos: test arc funcs were deleted + pow was implemented

--- src/main.c
+++ src/main.c
@@ -1,16 +1,22 @@
+#include <math.h>
 #include <stdio.h>
+
 #include "s21_math.h"
-#include "math.h"
 
-int main(){
+#define X 2.5, -0.4
 
-    printf("atan TESTS!!!\n\n Original: %lf\n ", atan(2));
-    printf("S21_test: %llf\n ", s21_atan_test(2));
-    printf("s21:      %llf\n", s21_atan(2));
-    //printf("TESTS!!!\n\n Original: %ld.\n S21: %ld\n\n\n", sin(-3), s21_sin_test(-3));
-    //printf("TESTS!!!\n\n Original: %ld.\n S21: %ld\n\n\n", s21_sin(-3), s21_sin(-3));
+int main() {
+  long double x = -2, z = 5;
+  printf("atan TESTS!!!\n\n Original: %.11Lf\n ", powl(X));
+  // printf("S21_test: %Lf\n ", s21_sqrt_test(x));
+  printf("s21:      %.11Lf\n", s21_pow(X));
+  // printf("TESTS!!!\n\n Original: %ld.\n S21: %ld\n\n\n", sin(-3),
+  // s21_sin_test(-3)); printf("TESTS!!!\n\n Original: %ld.\n S21: %ld\n\n\n",
+  // s21_sin(-3), s21_sin(-3));
 
-    return 0;
+  return 0;
 }
 
-//  gcc main.c s21_math.c -o math_test 
+//  gcc main.c s21_math.c -o math_test
+
+// s21_N_NAN s21_NAN s21_N_INF s21_INF
--- src/s21_math.h
+++ src/s21_math.h
@@ -1,9 +1,8 @@
 #pragma once
 
-#include <stdio.h>
 #include <limits.h>
 #include <stdint.h>
-
+#include <stdio.h>
 
 #define s21_PI 3.14159265358979324
 #define s21_N_PI -3.14159265358979324
@@ -15,15 +14,12 @@
 #define s21_N_NAN -0.0 / 0.0
 #define s21_MAX_double 1.7976931348623157e308
 #define EPSILON 1e-10
-#define s21_E        2.7182818284590452354
+#define s21_E 2.7182818284590452354
 
 long int s21_abs(int x);
 long double s21_acos(double x);
 long double s21_asin(double x);
 long double s21_atan(double x);
-long double s21_acos_test(double x);
-long double s21_asin_test(double x);
-long double s21_atan_test(double x);
 long double s21_ceil(double x);
 long double s21_cos(double x);
 long double s21_exp(double x);
@@ -31,6 +27,8 @@ long double s21_fabs(double x);
 long double s21_floor(double x);
 long double s21_fmod(double x, double y);
 long double s21_pow(double base, double exp);
+long double s21_pow_fraction(double base, double exp);
+long double s21_pow_binary(double base, double exp);
 long double s21_sin(double x);
 long double s21_sin_test(double x);
 long double s21_sqrt(double x);
@@ -39,8 +37,8 @@ long double s21_log(double x);
 int s21_factrial(int x);
 long double s21_sqrt_test(double x);
 
-//Function checks if double agruments are infinite, or other NaN values.
-// Exceptions: 0 - inf; 1 - neg inf; 2 - NaN; 3 - neg NaN
+// Function checks if double agruments are infinite, or other NaN values.
+//  Exceptions: 0 - inf; 1 - neg inf; 2 - NaN; 3 - neg NaN
 int input_check(double x1, double x2, double* exception1, double* exception2);
 double normalize_radian(double x);
 long double checker(double x);
